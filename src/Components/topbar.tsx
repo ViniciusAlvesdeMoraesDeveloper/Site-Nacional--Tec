5665ef0c28e76a0d9c88539eada0aabc12ad5672
Vinícius Alves <vinialvesmoraes18@gmail.com>
2025-09-03 14:52:33 -0300

    Atualização: link do sobre

--- src/Components/topbar.tsx
+++ src/Components/topbar.tsx
@@ -38,7 +38,7 @@ export default function TopBar() {
 
 
               <Link
-                href="#contato"
+                href="#sobre"
                 className="text-white hover:text-yellow-100 px-3 py-2 text-sm font-medium transition-colors"
               >
                 Sobre
